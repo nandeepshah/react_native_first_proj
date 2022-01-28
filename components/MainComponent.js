eaaa42e3726be89d6ef9e3f98b9eda0117bf979a
Nandeep Shah <nandeep.shah@gmail.com>
2022-01-28 17:11:53 -0500

    adding modal

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -187,8 +187,22 @@ const CustomDrawerContentComponent = props => (
 
 const MainNavigator = createDrawerNavigator(
 	{
-		Home: { screen: HomeNavigator },
-		Directory: { screen: DirectoryNavigator },
+		Home: {
+			screen: HomeNavigator,
+			navigationOptions: {
+				drawerIcon: ({ tintColor }) => (
+					<Icon name='home' type='font-awesome' size={24} color={tintColor} />
+				),
+			},
+		},
+		Directory: {
+			screen: DirectoryNavigator,
+			navigationOptions: {
+				drawerIcon: ({ tintColor }) => (
+					<Icon name='list' type='font-awesome' size={24} color={tintColor} />
+				),
+			},
+		},
 		Reservation: {
 			screen: ReservationNavigator,
 			navigationOptions: {
@@ -198,8 +212,34 @@ const MainNavigator = createDrawerNavigator(
 				),
 			},
 		},
-		About: { screen: AboutNavigator },
-		Contact: { screen: ContactNavigator },
+		About: {
+			screen: AboutNavigator,
+			navigationOptions: {
+				drawerLabel: 'About Us',
+				drawerIcon: ({ tintColor }) => (
+					<Icon
+						name='info-circle'
+						type='font-awesome'
+						size={24}
+						color={tintColor}
+					/>
+				),
+			},
+		},
+		Contact: {
+			screen: ContactNavigator,
+			navigationOptions: {
+				drawerLabel: 'Contact Us',
+				drawerIcon: ({ tintColor }) => (
+					<Icon
+						name='address-card'
+						type='font-awesome'
+						size={24}
+						color={tintColor}
+					/>
+				),
+			},
+		},
 	},
 	{
 		drawerBackgroundColor: '#CEC8FF',
