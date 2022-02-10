51f95584651c296ee7d76490741b3f487056ebad
Nandeep Shah <nandeep.shah@gmail.com>
2022-02-10 10:41:56 -0500

    adding login form with secure store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -20,6 +20,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoritesComponent';
+import Login from './LoginComponent';
 import { connect } from 'react-redux';
 import {
 	fetchCampsites,
@@ -191,6 +192,31 @@ const FavoritesNavigator = createStackNavigator(
 	}
 );
 
+const LoginNavigator = createStackNavigator(
+	{
+		Favorites: { screen: Login },
+	},
+	{
+		defaultNavigationOptions: ({ navigation }) => ({
+			headerStyle: {
+				backgroundColor: '#5637DD',
+			},
+			headerTintColor: '#fff',
+			headerTintStyle: {
+				color: '#fff',
+			},
+			headerLeft: (
+				<Icon
+					name='sign-in'
+					type='font-awesome'
+					iconStyle={styles.stackIcon}
+					onPress={() => navigation.toggleDrawer()}
+				/>
+			),
+		}),
+	}
+);
+
 const CustomDrawerContentComponent = props => (
 	<ScrollView>
 		<SafeAreaView
@@ -215,6 +241,19 @@ const CustomDrawerContentComponent = props => (
 
 const MainNavigator = createDrawerNavigator(
 	{
+		Login: {
+			screen: LoginNavigator,
+			navigationOptions: {
+				drawerIcon: ({ tintColor }) => (
+					<Icon
+						name='sign-in'
+						type='font-awesome'
+						size={24}
+						color={tintColor}
+					/>
+				),
+			},
+		},
 		Home: {
 			screen: HomeNavigator,
 			navigationOptions: {
@@ -279,6 +318,7 @@ const MainNavigator = createDrawerNavigator(
 		},
 	},
 	{
+		initialRouteName: 'Home',
 		drawerBackgroundColor: '#CEC8FF',
 		contentComponent: CustomDrawerContentComponent,
 	}
